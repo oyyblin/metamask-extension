f4abeb17a701aede6fb16c8ba6cfa2d230565264
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-10-09 18:12:08 +0100

    Add e2e test

--- test/e2e/tests/confirmations/transactions/erc20-token-send-redesign.spec.ts
+++ test/e2e/tests/confirmations/transactions/erc20-token-send-redesign.spec.ts
@@ -3,13 +3,16 @@ import { TransactionEnvelopeType } from '@metamask/transaction-controller';
 import { DAPP_URL } from '../../../constants';
 import { unlockWallet, WINDOW_TITLES } from '../../../helpers';
 import { Mockttp } from '../../../mock-e2e';
-import SetApprovalForAllTransactionConfirmation from '../../../page-objects/pages/set-approval-for-all-transaction-confirmation';
+import WatchAssetConfirmation from '../../../page-objects/pages/confirmations/legacy/watch-asset-confirmation';
+import HomePage from '../../../page-objects/pages/homepage';
+import SendTokenPage from '../../../page-objects/pages/send/send-token-page';
 import TestDapp from '../../../page-objects/pages/test-dapp';
 import GanacheContractAddressRegistry from '../../../seeder/ganache-contract-address-registry';
 import { Driver } from '../../../webdriver/driver';
 import { withRedesignConfirmationFixtures } from '../helpers';
 import { mocked4BytesSetApprovalForAll } from './erc721-revoke-set-approval-for-all-redesign';
 import { TestSuiteArguments } from './shared';
+import TokenTransferTransactionConfirmation from '../../../page-objects/pages/confirmations/redesign/token-transfer-confirmation';
 
 const { SMART_CONTRACTS } = require('../../../seeder/smart-contracts');
 
@@ -59,15 +62,31 @@ async function createTransactionAndAssertDetails(
 
   await testDapp.clickERC20WatchAssetButton();
 
-  await driver.delay(1024 ** 2);
+  await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
-  // await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+  const watchAssetConfirmation = new WatchAssetConfirmation(driver);
+  await watchAssetConfirmation.clickFooterConfirmButton();
 
-  // const setApprovalForAllConfirmation =
-  //   new SetApprovalForAllTransactionConfirmation(driver);
+  await driver.switchToWindowWithTitle(WINDOW_TITLES.ExtensionInFullScreenView);
 
-  // await setApprovalForAllConfirmation.check_revokeSetApprovalForAllTitle();
+  const homePage = new HomePage(driver);
+  await homePage.startSendFlow();
 
-  // await setApprovalForAllConfirmation.clickScrollToBottomButton();
-  // await setApprovalForAllConfirmation.clickFooterConfirmButton();
+  const sendToPage = new SendTokenPage(driver);
+  await sendToPage.check_pageIsLoaded();
+  await sendToPage.fillRecipient('0x2f318C334780961FB129D2a6c30D0763d9a5C970');
+  await sendToPage.fillAmount('1');
+
+  await sendToPage.click_assetPickerButton();
+  await sendToPage.click_secondTokenListButton();
+  await sendToPage.goToNextScreen();
+
+  const tokenTransferTransactionConfirmation =
+    new TokenTransferTransactionConfirmation(driver);
+  await tokenTransferTransactionConfirmation.check_walletInitiatedHeadingTitle();
+  await tokenTransferTransactionConfirmation.check_networkParagraph();
+  await tokenTransferTransactionConfirmation.check_interactingWithParagraph();
+  await tokenTransferTransactionConfirmation.check_networkFeeParagraph();
+
+  await tokenTransferTransactionConfirmation.clickFooterConfirmButton();
 }
